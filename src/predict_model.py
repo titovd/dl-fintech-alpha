cc3a87b9dc0908b1c569bb725732c8dbc54b931d
titovd <titov.dyu@phystech.edu>
2022-07-11 14:17:24 +0300

    predit model fixed

--- src/predict_model.py
+++ src/predict_model.py
@@ -20,15 +20,16 @@ def predict_rnn_model(
                            for x in os.listdir(cfg['test_buckets_path'])])
     
     accumulated_preds = None
-    for fold in tqdm(range(1, 4)):
+    for fold in tqdm(range(0, 4)):
         model = get_model_by_name(
             cfg['model']['name'], cfg['uniques_emb_path'], **cfg['model']['params'])
         
         model.load_state_dict(
             torch.load(os.path.join(cfg['path_to_checkpoints'], f"{fold}_fold_best_checkpoint.pt")))
         model.eval()
+        
         test_preds = inference(model, dataset_test, batch_size=256, device=cfg['device'])
-        if accumulate is None:
+        if accumulated_preds is None:
             accumulated_preds = test_preds
         else:
             accumulated_preds['score'] += test_preds['score']
